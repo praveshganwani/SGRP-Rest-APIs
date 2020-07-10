5126b22e0042578e9e3d24de233dc45df2d53970
csakshi17 <sakshichheda178@gmail.com>
2020-07-10 06:57:56 +0530

    Registration page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,11 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },  {
+    path: 'registration',
+    loadChildren: () => import('./registration/registration.module').then( m => m.RegistrationPageModule)
   },
+
  
   
  
