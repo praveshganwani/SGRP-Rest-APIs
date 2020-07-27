b450e0228ecd706867e646b445b533585e80377e
jainut20 <58257957+jainut20@users.noreply.github.com>
2020-07-28 03:11:54 +0530

    Added Detect Category feature

--- src/app/pages/lodge-complaint/lodge-complaint.page.ts
+++ src/app/pages/lodge-complaint/lodge-complaint.page.ts
@@ -34,21 +34,56 @@ export class LodgeComplaintPage implements OnInit {
       this.Categories = data
     })
   }
-  LodgeComplaint() {
-    try{
-    Notiflix.Loading.Standard();
-      this.web.post('grievances/grievance', this.grievance).subscribe((res: any) => {
-        Notiflix.Loading.Remove();
-        if (res.status == 1) {
-          this.showAlert('Sucess', 'Complaint logded successfully.')
-          this.grievance = {}
-        }
-        else{
-          this.showAlert('Failed', 'Something Went Wrong.')
-        }
-      })
+  DetectComplaint() {
+    try {
+      if (this.grievance.complaintDetail && this.grievance.complaintTitle) {
+        this.web.post('grievances/check/spam', { complaintDetail: this.grievance.complaintDetail }).subscribe((res: any) => {
+          console.log(res)
+          if (res.status == -100) {
+            Notiflix.Notify.Failure('Spam Complaints not allowed.Simplify your complaint')
+          }
+          else if (res.status == 1) {
+            this.web.post('grievances/check/category', { complaintDetail: this.grievance.complaintDetail }).subscribe((res: any) => {
+              console.log(res)
+              this.grievance.categoryId = res.categoryId
+            })
+          }
+        })
+      }
+      else {
+        Notiflix.Notify.Failure('Please add more detail to the complaint.')
+      }
     }
-    catch(err){
+    catch (err) {
+      console.log(err)
+    }
+  }
+  LodgeComplaint() {
+    try {
+      Notiflix.Loading.Standard();
+      if (this.grievance.complaintDetail.length < 15) {
+        Notiflix.Loading.Remove();
+        Notiflix.Notify.Warning('Please add more detail to the complaint.')
+      }
+      else if (this.grievance.complaintDetail && this.grievance.complaintTitle && this.grievance.categoryId && (this.grievance.complaintIsAnonymous == 0 || this.grievance.complaintIsAnonymous == 1)) {
+        this.web.post('grievances/grievance', this.grievance).subscribe((res: any) => {
+          Notiflix.Loading.Remove();
+          if (res.status == 1) {
+            this.showAlert('Sucess', 'Complaint logded successfully.')
+            this.grievance = {}
+          }
+          else {
+            this.showAlert('Failed', 'Something Went Wrong.')
+          }
+        })
+      }
+      else {
+        console.log(this.grievance)
+        Notiflix.Loading.Remove();
+        Notiflix.Notify.Warning('Empty Complaint Not allowed.Make sure your category is detected')
+      }
+    }
+    catch (err) {
       this.showAlert('Failed', 'Something Went Wrong.')
       Notiflix.Loading.Remove();
     }
